c76a263230ca8a06e7e8a091f1e720c709d477a0
louisebohlin <37780677+louisebohlin@users.noreply.github.com>
2018-09-17 13:35:13 +0200

    Done without stretch goals

--- code/script.js
+++ code/script.js
@@ -1,7 +1,11 @@
-// A function that adds and remove the class "active" on the section you click on.
-function toggle() {
+document.getElementById("section1").onclick = function() {
   this.classList.toggle("active")
 }
 
-// Selects and HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+document.getElementById("section2").onclick = function() {
+  this.classList.toggle("active")
+}
+
+document.getElementById("section3").onclick = function() {
+  this.classList.toggle("active")
+}
